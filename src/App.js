af526a2395d7da7a29aaa2f1504f74057571ca2f
Starlight Romero <starlightromero@gmail.com>
2020-10-28 07:55:41 -0700

    Imported Layout and BurgerBuilder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css'
+import Layout from './Components/Layout/Layout'
+import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder'
 
 function App () {
   return (
-    <div className='App' />
+    <div className='App'>
+      <Layout>
+        <BurgerBuilder />
+      </Layout>
+    </div>
   )
 }
 
