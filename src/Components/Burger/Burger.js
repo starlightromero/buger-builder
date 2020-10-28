bbfbaddad8bd471533c65ba5b7e058a64be71aa8
Starlight Romero <starlightromero@gmail.com>
2020-10-28 09:16:20 -0700

    added dynamic ingredients from BurgerBuilder state

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -5,15 +5,14 @@ import BurgerIngredient from './BurgerIngedient/BurgerIngredient'
 const burger = props => {
   const ingredients = Object.keys(props.ingredients).map(ingredient => {
     return [...Array(props.ingredients[ingredient])].map((_, i) => {
-      <BurgerIngredient key={`${ingredient}${i}`} type={ingredient} />
+      return <BurgerIngredient key={`${ingredient}${i}`} type={ingredient} />
     })
   })
 
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
-      <BurgerIngredient type='cheese' />
-      <BurgerIngredient type='meat' />
+      {ingredients}
       <BurgerIngredient type='bread-bottom' />
     </div>
   )
