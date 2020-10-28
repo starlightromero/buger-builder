1b6c827b2866d2f5ef2442e69f05191b52133cb1
Starlight Romero <starlightromero@gmail.com>
2020-10-28 08:07:40 -0700

    added dyanmic ingredient component

---
+++ src/Components/Burger/BurgerIngedient/BurgerIngredient.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import classes from './BurgerIngredient.css'
+
+const burgerIngredients = props => {
+  let ingredient = null
+
+  switch (props.type) {
+    case ('bread-bottom'):
+      ingredient = <div className={classes.BreadButtom} />
+      break
+    case ('bread-top'):
+      ingredient = (
+        <div className={classes.BreadTop}>
+          <div className={classes.Seeds1} />
+          <div className={classes.Seeds2} />
+        </div>
+      )
+      break
+    case ('meat'):
+      ingredient = <div className={classes.Meat} />
+      break
+    case ('cheese'):
+      ingredient = <div className={classes.Meat} />
+      break
+    case ('bacon'):
+      ingredient = <div className={classes.Bacon} />
+      break
+    case ('salad'):
+      ingredient = <div className={classes.Salad} />
+      break
+    default:
+      break
+  }
+
+  return (
+    <div>MyComponent</div>
+  )
+}
+
+export default burgerIngredients
