ba2fddf06a71d38fb203efd560f0d485f07bfcdd
Starlight Romero <starlightromero@gmail.com>
2020-10-28 08:20:48 -0700

    added missing return

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from 'react'
 import classes from './Layout.css'
 
 const layout = props => {
-  <>
-    <div>Toolbar, SideDraw, Backdrop</div>
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </>
+  return (
+    <>
+      <div>Toolbar, SideDraw, Backdrop</div>
+      <main className={classes.Content}>
+        {props.children}
+      </main>
+    </>
+  )
 }
 
 export default layout
