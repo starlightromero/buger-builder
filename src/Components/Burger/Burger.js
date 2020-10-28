65dba28c577af8a6a90b3b57190a6d287244711c
Starlight Romero <starlightromero@gmail.com>
2020-10-28 09:14:56 -0700

    map the BurgerIngredient from the BurgerBuilder state

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -3,6 +3,12 @@ import classes from './Burger.module.css'
 import BurgerIngredient from './BurgerIngedient/BurgerIngredient'
 
 const burger = props => {
+  const ingredients = Object.keys(props.ingredients).map(ingredient => {
+    return [...Array(props.ingredients[ingredient])].map((_, i) => {
+      <BurgerIngredient key={`${ingredient}${i}`} type={ingredient} />
+    })
+  })
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react'
 import Burger from '../../Components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2,
+    }
+  }
+  
   render () {
     return (
       <>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </>
     )
