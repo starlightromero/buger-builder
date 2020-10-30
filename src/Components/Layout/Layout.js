43c82604df46a2da444f951bcae880e96ce6ed7d
Starlight Romero <starlightromero@gmail.com>
2020-10-30 10:34:56 -0700

    added SideDrawer

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = props => {
   return (
     <>
       <Toolbar />
+      <SideDrawer />
       <div>SideDraw, Backdrop</div>
       <main className={classes.Content}>
         {props.children}
