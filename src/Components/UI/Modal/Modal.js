748441d4b8234ed8d8ee912c69b5c6d631e20d0d
Starlight Romero <starlightromero@gmail.com>
2020-10-30 14:38:00 -0700

    changed Modal to class based and added shouldComponentUpdate

--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classes from './Modal.module.css'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = props => {
-  return (
-    <>
-      <Backdrop show={props.show} clicked={props.modalClosed}></Backdrop>
+class Modal extends Component {
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show
+  }
+
+  render() {
+    return (
+      <>
+      <Backdrop show={this.props.show} clicked={this.props.modalClosed}></Backdrop>
       <div
         className={classes.Modal}
         style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? 1 : 0
+          transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: this.props.show ? 1 : 0
         }}
-      >
-        {props.children}
+        >
+        {this.props.children}
       </div>
-    </>
-  )
+      </>
+    )
+  }
 }
 
-export default modal
+export default Modal
