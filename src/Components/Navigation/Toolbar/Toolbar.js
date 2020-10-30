931efa873e8ac07be47cc1ddd3d2ee083c86d097
Starlight Romero <starlightromero@gmail.com>
2020-10-30 10:35:51 -0700

    added dynamic sized logo and NavigationItems component

--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import classes from './Toolbar.module.css'
 import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolbar = () => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <Logo />
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
     <nav>
-      ...
+      <NavigationItems />
     </nav>
   </header>
 )
