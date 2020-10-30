842208ec18e5deb5d8b46c096f39646de75e142b
Starlight Romero <starlightromero@gmail.com>
2020-10-29 20:05:01 -0700

    added Order Now button

--- src/Components/Burger/BuildControls/BuildControls.js
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -22,6 +22,7 @@ const buildControls = props => {
           disabled={props.disabled[control.type]}
         />
       ))}
+      <button className={classes.OrderButton} disabled={!props.purchasable}>ORDER NOW</button>
     </div>
   )
 }
