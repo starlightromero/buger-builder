1cd331b5398cfc2962b67f0f6d95280c21a9c0c4
Starlight Romero <starlightromero@gmail.com>
2020-11-01 19:24:50 -0800

    removed unused curly braces

--- src/Components/Burger/BuildControls/BuildControls.js
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -9,28 +9,26 @@ const controls = [
   { label: 'Meat', type: 'meat' }
 ]
 
-const buildControls = props => {
-  return (
-    <div className={classes.BuildControls}>
-      <p>Current Price: <strong>${props.price.toFixed(2)}</strong></p>
-      {controls.map(control => (
-        <BuildControl
-          key={control.label}
-          label={control.label}
-          added={() => props.ingredientAdded(control.type)}
-          removed={() => props.ingredientRemoved(control.type)}
-          disabled={props.disabled[control.type]}
-        />
-      ))}
-      <button
-        className={classes.OrderButton}
-        disabled={!props.purchasable}
-        onClick={props.ordered}
-      >
-        ORDER NOW
-      </button>
-    </div>
-  )
-}
+const buildControls = props => (
+  <div className={classes.BuildControls}>
+    <p>Current Price: <strong>${props.price.toFixed(2)}</strong></p>
+    {controls.map(control => (
+      <BuildControl
+        key={control.label}
+        label={control.label}
+        added={() => props.ingredientAdded(control.type)}
+        removed={() => props.ingredientRemoved(control.type)}
+        disabled={props.disabled[control.type]}
+      />
+    ))}
+    <button
+      className={classes.OrderButton}
+      disabled={!props.purchasable}
+      onClick={props.ordered}
+    >
+      ORDER NOW
+    </button>
+  </div>
+)
 
 export default buildControls
