cb83c426303fc9d77eebd38642dab15753377869
Starlight Romero <starlightromero@gmail.com>
2020-11-04 11:55:49 -0800

    added Checkout component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
 import Layout from './Containers/Layout/Layout'
 import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder'
+import Checkout from './Containers/Checkout/Checkout'
 
 function App () {
   return (
     <div className='App'>
       <Layout>
         <BurgerBuilder />
+        <Checkout />
       </Layout>
     </div>
   )
