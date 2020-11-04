465cbab9f5ea4b0314440b9d4a6ef2e27fe82b30
Starlight Romero <starlightromero@gmail.com>
2020-11-04 12:04:09 -0800

    added routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css'
 import Layout from './Containers/Layout/Layout'
 import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder'
@@ -5,12 +6,12 @@ import Checkout from './Containers/Checkout/Checkout'
 
 function App () {
   return (
-    <div className='App'>
+    <Router className='App'>
       <Layout>
-        <BurgerBuilder />
-        <Checkout />
+        <Route path='/checkout' component={Checkout} />
+        <Route path='/' exact component={BurgerBuilder} />
       </Layout>
-    </div>
+    </Router>
   )
 }
 
