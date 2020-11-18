bfb200c5e543a422c5283f91ee54971cb0e9b34a
Starlight Romero <starlightromero@gmail.com>
2020-11-18 08:33:38 -0800

    checkout routes to ContactData

--- src/Containers/Checkout/Checkout.js
+++ src/Containers/Checkout/Checkout.js
@@ -1,42 +1,53 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import CheckoutSummary from '../../Components/Order/CheckoutSummary/CheckoutSummary'
+import ContactData from './ContactData/ContactData'
 
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 1,
-      meat: 1,
-      cheese: 1,
-      bacon: 1,
-    }
+    ingredients: null,
+    price: 0
   }
 
-  componentDidMount () {
+  componentWillMount () {
     const query = new URLSearchParams(this.props.location.search)
     const ingredients = {}
+    let price = 0
     for (let param of query.entries()) {
-      ingredients[param[0]] = +param[1]
+      if (param[0] === 'price') {
+        price = param[1]
+      } else {
+        ingredients[param[0]] = +param[1]
+      }
     }
-    this.setState({ingredients: ingredients})
+    this.setState({ingredients: ingredients, totalPrice: price})
   }
 
   checkoutCanceledHandler = () => {
     this.props.history.goBack()
   }
 
-  checkoutContinuedHandler = () => {
+  checkoutContinuedHandler = event => {
+    event.preventDefault()
     this.props.history.replace('/checkout/contact-data')
   }
   
   render () {
-    const { ingredients } = this.state
+    const { ingredients, totalPrice } = this.state
     return (
       <div>
         <CheckoutSummary
           checkoutCanceled={this.checkoutCanceledHandler}
           checkoutContinued={this.checkoutContinuedHandler}
-          ingredients={ingredients}
-        />
+          ingredients={ingredients} />
+        <Route
+          path={this.props.match.path + '/contact-data'}
+          render={(props) => (
+            <ContactData
+              ingredients={ingredients}
+              price={totalPrice}
+              {...props} />
+          )} />
       </div>
     )
   }
