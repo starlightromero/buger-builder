7ad9397591633c9a760d648ba4439b9385b9da39
Starlight Romero <starlightromero@gmail.com>
2020-11-18 08:49:13 -0800

    added Orders route

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css'
 import Layout from './Containers/Layout/Layout'
 import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './Containers/Checkout/Checkout'
+import Orders from './Containers/Orders/Orders'
 
 function App () {
   return (
     <Router className='App'>
       <Layout>
         <Route path='/checkout' component={Checkout} />
+        <Route path='/orders' component={Orders} />
         <Route path='/' exact component={BurgerBuilder} />
       </Layout>
     </Router>
