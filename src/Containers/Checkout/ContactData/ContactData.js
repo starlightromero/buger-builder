2330d6d6846c640ea145ad65df201fe92e2b15fe
Starlight Romero <starlightromero@gmail.com>
2020-11-20 11:52:21 -0800

    add orderData to post request on successful form submit

--- src/Containers/Checkout/ContactData/ContactData.js
+++ src/Containers/Checkout/ContactData/ContactData.js
@@ -65,10 +65,15 @@ class ContactData extends Component {
   orderHandler = event => {
     event.preventDefault()
     this.setState({ loading: true })
+    const orderData = {}
+    for (let formElementIdentifier in this.state.orderForm) {
+      orderData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
+    }
     const { ingredients, price, history } = this.props
     axios.post('orders.json', {
       ingredients,
       price,
+      orderData,
     }).then(response => {
       this.setState({ loading: false })
       history.push('/')
@@ -98,7 +103,7 @@ class ContactData extends Component {
       })
     }
     let form = (
-      <form>
+      <form onSubmit={this.orderHandler}>
         {formElementsArray.map(formElement => (
           <Input
             key={formElement.id}
@@ -107,7 +112,7 @@ class ContactData extends Component {
             value={formElement.config.value}
             changed={event => this.inputChangedHandler(event, formElement.id)} />
         ))}
-        <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+        <Button btnType="Success">ORDER</Button>
       </form>
     )
     if (this.state.loading === true) {
