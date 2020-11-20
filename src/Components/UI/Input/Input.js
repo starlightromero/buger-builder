0e07b4ab8d86f4b2861a969346e9347e964f6a47
Starlight Romero <starlightromero@gmail.com>
2020-11-20 12:46:42 -0800

    added validation error

--- src/Components/UI/Input/Input.js
+++ src/Components/UI/Input/Input.js
@@ -51,10 +51,20 @@ const Input = props => {
       )
   }
 
+  let validationError = null
+  if (props.invalid && props.touched) {
+    validationError = (
+      <p className={classes.ValidationError}>
+        Please enter a valid {props.valueType}
+      </p>
+    )
+  }
+
   return (
     <div className={classes.Input}>
       <label className={classes.Label}>{props.label}</label>
       {inputElement}
+      {validationError}
     </div>
   )
 }
