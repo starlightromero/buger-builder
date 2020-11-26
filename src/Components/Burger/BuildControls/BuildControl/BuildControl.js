929c34b5f0b1b1ca0ea069e2d4130d166de25677
Starlight Romero <starlightromero@gmail.com>
2020-11-26 02:36:40 -0800

    fixed build controls alignment

--- src/Components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/Components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,12 +4,14 @@ import classes from './BuildControl.module.css'
 const buildControl = props => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
-    <button className={classes.Less} onClick={props.removed} disabled={props.disabled}>
-      Less
-    </button>
-    <button className={classes.More} onClick={props.added}>
-      More
-    </button>
+    <div className={classes.Buttons}>
+      <button className={classes.Less} onClick={props.removed} disabled={props.disabled}>
+        Less
+      </button>
+      <button className={classes.More} onClick={props.added}>
+        More
+      </button>
+    </div>
   </div>
 )
 
--- src/Components/Burger/BuildControls/BuildControls.js
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -17,15 +17,17 @@ const controls = [
 const buildControls = props => (
   <div className={classes.BuildControls}>
     <p>Current Price: <strong>${props.price.toFixed(2)}</strong></p>
-    {controls.map(control => (
-      <BuildControl
-        key={control.label}
-        label={control.label}
-        added={() => props.ingredientAdded(control.type)}
-        removed={() => props.ingredientRemoved(control.type)}
-        disabled={props.disabled[control.type]}
-      />
-    ))}
+    <div>
+      {controls.map(control => (
+        <BuildControl
+          key={control.label}
+          label={control.label}
+          added={() => props.ingredientAdded(control.type)}
+          removed={() => props.ingredientRemoved(control.type)}
+          disabled={props.disabled[control.type]}
+        />
+      ))}
+    </div>
     <button
       className={classes.OrderButton}
       disabled={!props.purchasable}
