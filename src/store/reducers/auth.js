ed81d112354b8009ac724e1276f7f39444647f7e
Starlight Romero <starlightromero@gmail.com>
2020-11-26 13:50:49 -0800

    added initial state

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -7,7 +7,7 @@ const initialState = {
   loading: false
 }
 
-const reducer = (state, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START:
       return {
