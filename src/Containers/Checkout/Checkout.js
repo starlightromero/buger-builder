3091deb336412af0c26067d9a24966d68bff7787
Starlight Romero <starlightromero@gmail.com>
2020-11-25 17:18:38 -0800

    added mapStateToProps and removed state

--- src/Containers/Checkout/Checkout.js
+++ src/Containers/Checkout/Checkout.js
@@ -2,26 +2,9 @@ import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import CheckoutSummary from '../../Components/Order/CheckoutSummary/CheckoutSummary'
 import ContactData from './ContactData/ContactData'
+import { connect } from 'react-redux'
 
 class Checkout extends Component {
-  state = {
-    ingredients: null,
-    price: 0
-  }
-
-  componentWillMount () {
-    const query = new URLSearchParams(this.props.location.search)
-    const ingredients = {}
-    let price = 0
-    for (let param of query.entries()) {
-      if (param[0] === 'price') {
-        price = param[1]
-      } else {
-        ingredients[param[0]] = +param[1]
-      }
-    }
-    this.setState({ingredients: ingredients, totalPrice: price})
-  }
 
   checkoutCanceledHandler = () => {
     this.props.history.goBack()
@@ -33,7 +16,7 @@ class Checkout extends Component {
   }
   
   render () {
-    const { ingredients, totalPrice } = this.state
+    const { ingredients } = this.props
     return (
       <div>
         <CheckoutSummary
@@ -42,15 +25,16 @@ class Checkout extends Component {
           ingredients={ingredients} />
         <Route
           path={this.props.match.path + '/contact-data'}
-          render={(props) => (
-            <ContactData
-              ingredients={ingredients}
-              price={totalPrice}
-              {...props} />
-          )} />
+          component={ContactData} />
       </div>
     )
   }
 }
 
-export default Checkout
+const mapStateToProps = state => {
+  return {
+    ingredients: state.ingredients,
+  }
+}
+
+export default connect(mapStateToProps)(Checkout)
