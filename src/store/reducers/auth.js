80af4ef170c42c614d487c1aabcc5033348fe549
Starlight Romero <starlightromero@gmail.com>
2020-11-26 14:32:12 -0800

    added auth logout reducer

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -29,6 +29,12 @@ const reducer = (state = initialState, action) => {
         error: action.error,
         loading: false
       }
+    case actionTypes.AUTH_LOGOUT:
+      return {
+        ...state,
+        token: null,
+        userId: null
+      }
     default:
       return state
   }
