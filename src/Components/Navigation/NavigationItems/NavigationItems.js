14069203e757cd85b1bee7755c963bd536183dc2
Starlight Romero <starlightromero@gmail.com>
2020-11-26 15:44:46 -0800

    fixed nav items auth bug

--- src/Components/Navigation/NavigationItems/NavigationItems.js
+++ src/Components/Navigation/NavigationItems/NavigationItems.js
@@ -3,20 +3,22 @@ import classes from './NavigationItems.module.css'
 import NavigationItem from './NavigationItem/NavigationItem'
 
 const navigationItems = props => {
-  let authNavItem = (
+  let authNavItems = (
     <NavigationItem link='/auth'>Authenticate</NavigationItem>
   )
-  if (!props.isAuth) {
-    authNavItem = (
-      <NavigationItem link='/logout'>Logout</NavigationItem>
+  if (props.isAuth) {
+    authNavItems = (
+      <>
+        <NavigationItem link='/orders'>Orders</NavigationItem>
+        <NavigationItem link='/logout'>Logout</NavigationItem>
+      </>
     )
   }
 
   return (
     <ul className={classes.NavigationItems}>
       <NavigationItem link='/' exact>Burger Builder</NavigationItem>
-      <NavigationItem link='/orders'>Orders</NavigationItem>
-      {authNavItem}
+      {authNavItems}
     </ul>
   )
 }
