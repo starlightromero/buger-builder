9c4201391042a6ef487d6721c8b62a3d654d8ac0
Starlight Romero <starlightromero@gmail.com>
2020-11-26 13:11:48 -0800

    exported auth

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,6 +1,3 @@
-export {
-  addIngredient,
-  removeIngredient,
-  initIngredients
-} from './burgerBuilder'
+export { addIngredient, removeIngredient, initIngredients } from './burgerBuilder'
 export { purchaseBurger, purchaseInit, fetchOrders } from './order'
+export { auth } from './auth'
