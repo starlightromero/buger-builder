067e411dae3c45e37a28f358e849bb8fb1645969
Starlight Romero <starlightromero@gmail.com>
2020-11-26 17:23:52 -0800

    added isAuthenticated prop to dynamically display routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Route, withRouter, Switch, Redirect } from 'react-router-dom'
 import './App.css'
 import Layout from './Containers/Layout/Layout'
 import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder'
@@ -16,17 +16,39 @@ class App extends Component {
   }
 
   render () {
+    let routes = (
+      <Switch>
+        <Route exact path='/auth' component={Auth} />
+        <Route exact path='/' component={BurgerBuilder} />
+        <Redirect to='/' />
+      </Switch>
+    )
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route exact path='/checkout' component={Checkout} />
+          <Route exact path='/orders' component={Orders} />
+          <Route exact path='/logout' component={Logout} />
+          <Route exact path='/' component={BurgerBuilder} />
+          <Redirect to='/' />
+        </Switch>
+      )
+    }
+
     return (
       <Router className='App'>
         <Layout>
-          <Route exact path='/checkout' component={Checkout} />
-          <Route exact path='/orders' component={Orders} />
-          <Route exact path='/auth' component={Auth} />
-          <Route exact path='/logout' component={Logout} />
-          <Route exact path='/' component={BurgerBuilder} />
+          {routes}
         </Layout>
       </Router>
     )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
   }
 }
 
@@ -36,4 +58,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(App))
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
