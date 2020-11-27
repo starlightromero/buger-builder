45930bfdbb8ed0d849f72e6b9863af096c097b1f
Starlight Romero <starlightromero@gmail.com>
2020-11-27 09:07:20 -0800

    added checkAuthTimeoutSaga

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,11 +1,15 @@
+import { delay } from 'redux-saga/effects'
 import { put } from 'redux-saga/effects'
-import * as actionTypes from '../actions/actionTypes'
+import * as actions from '../actions'
 
 export function* logoutSaga (action) {
   yield localStorage.removeItem('token')
   yield localStorage.removeItem('expirationTime')
   yield localStorage.removeItem('userId')
-  yield put({
-    type: actionTypes.AUTH_LOGOUT
-  })
+  yield put(actions.logoutSuccess())
+}
+
+export function* checkAuthTimeoutSaga (action) {
+  yield delay(action.expirationTime * 1000)
+  yield put(actions.logout())
 }
