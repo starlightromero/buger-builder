a320b7cfac92ba893bf9a03a00ea67dca21ab7b0
Starlight Romero <starlightromero@gmail.com>
2020-11-26 17:46:52 -0800

    added validation helper func

--- src/Containers/Auth/Auth.js
+++ src/Containers/Auth/Auth.js
@@ -6,6 +6,7 @@ import Input from '../../Components/UI/Input/Input'
 import Button from '../../Components/UI/Button/Button'
 import Loader from '../../Components/UI/Loader/Loader'
 import * as actions from '../../store/actions'
+import { checkValidity } from '../../shared/validation'
 
 class Auth extends Component {
   state = {
@@ -66,32 +67,6 @@ class Auth extends Component {
     })
   }
 
-  checkValidity = (value, rules) => {
-    let isValid = true
-    if (!rules) {
-      return true
-    }
-    if(rules.required) {
-      isValid = value.trim() !== '' && isValid
-    }
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid
-    }
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid
-    }
-    if (rules.isEmail) {
-      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-      isValid = pattern.test(value) && isValid
-    }
-    if (rules.isNumeric) {
-      const pattern = /^\d+$/;
-      isValid = pattern.test(value) && isValid
-    }
-
-    return isValid
-  }
-
   inputChangedHandler = (event, inputIdentifier) => {
     const updatedControls = {
       ...this.state.controls
@@ -100,7 +75,7 @@ class Auth extends Component {
       ...updatedControls[inputIdentifier]
     }
     updatedFormElement.value = event.target.value
-    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+    updatedFormElement.valid = checkValidity(updatedFormElement.value, updatedFormElement.validation)
     updatedFormElement.touched = true
     updatedControls[inputIdentifier] = updatedFormElement
 
--- src/Containers/Checkout/ContactData/ContactData.js
+++ src/Containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,7 @@ import Input from '../../../Components/UI/Input/Input'
 import withErrorHandler from '../../../HOC/withErrorHandler/withErrorHandler'
 import { connect } from 'react-redux'
 import * as actions from '../../../store/actions'
+import { checkValidity } from '../../../shared/validation'
 
 class ContactData extends Component {
   state = {
@@ -112,32 +113,6 @@ class ContactData extends Component {
     onOrderBurger(order, token)
   }
 
-  checkValidity = (value, rules) => {
-    let isValid = true
-    if (!rules) {
-      return true
-    }
-    if(rules.required) {
-      isValid = value.trim() !== '' && isValid
-    }
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid
-    }
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid
-    }
-    if (rules.isEmail) {
-      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-      isValid = pattern.test(value) && isValid
-    }
-    if (rules.isNumeric) {
-      const pattern = /^\d+$/;
-      isValid = pattern.test(value) && isValid
-    }
-
-    return isValid
-  }
-
   inputChangedHandler = (event, inputIdentifier) => {
     const updatedOrderForm = {
       ...this.state.orderForm
@@ -146,7 +121,7 @@ class ContactData extends Component {
       ...updatedOrderForm[inputIdentifier]
     }
     updatedFormElement.value = event.target.value
-    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+    updatedFormElement.valid = checkValidity(updatedFormElement.value, updatedFormElement.validation)
     updatedFormElement.touched = true
     updatedOrderForm[inputIdentifier] = updatedFormElement
 
