1f0245b5c2aa5e7b9d9bcebd3e58bef67783382c
Starlight Romero <starlightromero@gmail.com>
2020-11-27 09:06:59 -0800

    added checkAuthTimeoutSaga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,7 +1,8 @@
 import { takeEvery } from 'redux-saga/effects'
 import * as actionTypes from '../actions/actionTypes'
-import { logoutSaga } from './auth'
+import { logoutSaga, checkAuthTimeoutSaga } from './auth'
 
 export function* watchAuth () {
   yield takeEvery(actionTypes.AUTH_INIT_LOGOUT, logoutSaga)
+  yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga)
 }
