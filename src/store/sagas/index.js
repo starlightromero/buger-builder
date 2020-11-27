64bb7b0445015175e869fd94f8ad69f16379cd53
Starlight Romero <starlightromero@gmail.com>
2020-11-27 09:44:33 -0800

    added takeEvery for authCheckStateSaga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,9 +1,15 @@
 import { takeEvery } from 'redux-saga/effects'
 import * as actionTypes from '../actions/actionTypes'
-import { logoutSaga, checkAuthTimeoutSaga, authUserSaga } from './auth'
+import {
+  logoutSaga,
+  checkAuthTimeoutSaga,
+  authUserSaga,
+  authCheckStateSaga
+} from './auth'
 
 export function* watchAuth () {
   yield takeEvery(actionTypes.AUTH_INIT_LOGOUT, logoutSaga)
   yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga)
   yield takeEvery(actionTypes.AUTH_USER, authUserSaga)
+  yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
 }
