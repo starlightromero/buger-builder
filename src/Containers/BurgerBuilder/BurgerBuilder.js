887720a98c8ec5078d3824763d305f174cd39929
Starlight Romero <starlightromero@gmail.com>
2020-11-26 16:26:21 -0800

    send user to auth route if they are not signed in and try to order

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -28,7 +28,11 @@ class BurgerBuilder extends Component {
   }
 
   purchaseHandler = () => {
-    this.setState({ purchasing: true })
+    if (this.props.isAuthenticated) {
+      this.setState({ purchasing: true })
+    } else {
+      this.props.history.push('/auth')
+    }
   }
 
   purchaseCancelHandler = () => {
