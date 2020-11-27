de886bbdbad1259a0dc8017479d0f924903764e5
Starlight Romero <starlightromero@gmail.com>
2020-11-27 10:02:43 -0800

    added watchBurgerBuilder

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -6,10 +6,15 @@ import {
   authUserSaga,
   authCheckStateSaga
 } from './auth'
+import { initIngredientsSaga } from './burgerBuilder'
 
 export function* watchAuth () {
   yield takeEvery(actionTypes.AUTH_INIT_LOGOUT, logoutSaga)
   yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga)
   yield takeEvery(actionTypes.AUTH_USER, authUserSaga)
   yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
+}
+
+export function* watchBurgerBuilder () {
+  yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga)
 }
