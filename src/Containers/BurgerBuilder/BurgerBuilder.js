0296e3942aa8f017adc32809699d90743aead1d6
Starlight Romero <starlightromero@gmail.com>
2020-11-26 16:40:54 -0800

    added onInitPurchase and onSetAuthRedirectPath

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -31,6 +31,7 @@ class BurgerBuilder extends Component {
     if (this.props.isAuthenticated) {
       this.setState({ purchasing: true })
     } else {
+      this.props.onSetAuthRedirectPath('/checkout')
       this.props.history.push('/auth')
     }
   }
@@ -106,7 +107,8 @@ const mapDispatchToProps = dispatch => {
     onIngredientAdded: ingredient => dispatch(actions.addIngredient(ingredient)),
     onIngredientRemoved: ingredient => dispatch(actions.removeIngredient(ingredient)),
     onInitIngredients: () => dispatch(actions.initIngredients()),
-    onInitPurchase: () => dispatch(actions.purchaseInit())
+    onInitPurchase: () => dispatch(actions.purchaseInit()),
+    onSetAuthRedirectPath: path => dispatch(actions.setAuthRedirectPath(path))
   }
 }
 
