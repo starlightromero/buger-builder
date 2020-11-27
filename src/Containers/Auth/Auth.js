390bf0c9f33b5d251cade4868a6d76f859cb49ce
Starlight Romero <starlightromero@gmail.com>
2020-11-26 16:41:24 -0800

    added ComponentDidMount, additional props, and additional dispatch props

--- src/Containers/Auth/Auth.js
+++ src/Containers/Auth/Auth.js
@@ -43,6 +43,12 @@ class Auth extends Component {
     isSignUp: false,
   }
 
+  componentDidMount () {
+    if (!this.props.buildingBurger && this.props.authRedirectPath !== '/') {
+      this.props.nSetAuthRedirectPath('/')
+    }
+  }
+
   submitHandler = event => {
     event.preventDefault()
     this.props.onAuth(
@@ -155,7 +161,7 @@ class Auth extends Component {
 
     let authRedirect = null
     if (this.props.isAuthenticated) {
-      authRedirect = <Redirect to='/' />
+      authRedirect = <Redirect to={() => this.props.onAuthRedirectPath('/')} />
     }
     
     return (
@@ -175,13 +181,16 @@ const mapStateToProps = state => {
   return {
     loading: state.auth.loading,
     error: state.auth.error,
-    isAuthenticated: state.auth.token !== null
+    isAuthenticated: state.auth.token !== null,
+    buildingBurger: state.burgerBuilder.building,
+    authRedirectPath: state.auth.authRedirectPath
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
+    onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp)),
+    onSetAuthRedirectPath: path => dispatch(actions.setAuthRedirectPath(path))
   }
 }
 
