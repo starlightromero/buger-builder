6b0e02728f39820c3a87060f836f46fb3195faed
Starlight Romero <starlightromero@gmail.com>
2020-11-27 10:58:26 -0800

    moved bulk of logic into sagas

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -23,13 +23,10 @@ export const purchaseBurgerStart = () => {
 }
 
 export const purchaseBurger = (orderData, token) => {
-  return dispatch => {
-    dispatch(purchaseBurgerStart())
-    axios.post(`orders.json?auth=${token}`, orderData).then(response => {
-      dispatch(purchaseBurgerSuccess(response.data.name, orderData))
-    }).catch(error => {
-      dispatch(purchaseBurgerFail(error))
-    })
+  return {
+    type: actionTypes.PURCHASE_BURGER,
+    orderData,
+    token
   }
 }
 
@@ -46,7 +43,7 @@ export const fetchOrdersSuccess = orders => {
   }
 }
 
-export const fecthOrdersFail = error => {
+export const fetchOrdersFail = error => {
   return {
     type: actionTypes.FETCH_ORDERS_FAIL,
     error: error
@@ -60,20 +57,9 @@ export const fetchOrdersStart = () => {
 }
 
 export const fetchOrders = (token, userId) => {
-  return dispatch => {
-    dispatch(fetchOrdersStart())
-    axios.get(
-      `/orders.json?auth=${token}&orderBy="userId"&equalTo="${userId}"`
-    ).then(response => {
-      const fetchedOrders = []
-      for (let key in response.data) {
-        fetchedOrders.push({
-          id: key, ...response.data[key]
-        })
-      }
-      dispatch(fetchOrdersSuccess(fetchedOrders))
-    }).catch(error => {
-      dispatch(fetchOrdersFailed(error))
-    })
+  return {
+    type: actionTypes.FETCH_ORDERS,
+    token,
+    userId
   }
 }
