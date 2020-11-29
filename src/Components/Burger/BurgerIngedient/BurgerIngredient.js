72fb7051433f61def0090db890d2d08f2e9c3e73
Starlight Romero <starlightromero@gmail.com>
2020-11-29 14:09:59 -0800

    fixed casing

--- src/Components/Burger/BurgerIngedient/BurgerIngredient.js
+++ src/Components/Burger/BurgerIngedient/BurgerIngredient.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classes from './BurgerIngredient.module.css'
 
-const burgerIngredient = props => {
+const BurgerIngredient = props => {
   let ingredient = null
 
   switch (props.type) {
@@ -51,8 +51,8 @@ const burgerIngredient = props => {
   return ingredient
 }
 
-burgerIngredient.propTypes = {
+BurgerIngredient.propTypes = {
   type: PropTypes.string.isRequired
 }
 
-export default burgerIngredient
+export default BurgerIngredient
