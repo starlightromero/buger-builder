5016d37fac078e538bb3b487564adc244de9fdd5
Starlight Romero <starlightromero@gmail.com>
2020-12-02 07:58:24 -0800

    addded PropTypes

--- src/Components/UI/Button/Button.js
+++ src/Components/UI/Button/Button.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import classes from './Button.module.css'
 
 const Button = props => (
   <button
     disabled={props.disabled}
     className={[classes.Button, classes[props.btnType]].join(' ')}
-    onClick={props.clicked}
-  >
+    onClick={props.clicked}>
     {props.children}
   </button>
 )
+
+Button.propTypes = {
+  disabled: PropTypes.bool,
+  clicked: PropTypes.func,
+  children: PropTypes.string,
+  btnType: PropTypes.string
+}
 
 export default Button
