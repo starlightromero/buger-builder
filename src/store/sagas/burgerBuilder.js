2e9cce30badbe3f6576b9963db5abe8129d38efb
Starlight Romero <starlightromero@gmail.com>
2020-12-02 07:56:54 -0800

    linted

--- src/store/sagas/burgerBuilder.js
+++ src/store/sagas/burgerBuilder.js
@@ -2,7 +2,7 @@ import axios from '../../axios-orders'
 import { put } from 'redux-saga/effects'
 import * as actions from '../actions'
 
-export function* initIngredientsSaga (action) {
+export function * initIngredientsSaga (action) {
   try {
     const response = yield axios.get('ingredients.json')
     yield put(actions.setIngredients(response.data))
