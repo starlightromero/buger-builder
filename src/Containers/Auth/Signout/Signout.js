42beb40537ed644192c9535fbfd8db5069d2cce5
Starlight Romero <starlightromero@gmail.com>
2020-12-02 09:49:15 -0800

    used hooks instead of connect

--- src/Containers/Auth/Signout/Signout.js
+++ src/Containers/Auth/Signout/Signout.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react'
 import { Redirect } from 'react-router-dom'
-import { connect } from 'react-redux'
-import PropTypes from 'prop-types'
+import { useDispatch } from 'react-redux'
 import * as actions from '../../../store/actions'
 
 const Signout = props => {
-  const { onSignout } = props
+  const dispatch = useDispatch()
+  const onSignout = () => dispatch(actions.signout())
 
   useEffect(() => {
     onSignout()
@@ -14,14 +14,4 @@ const Signout = props => {
   return <Redirect to='/' />
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    onSignout: () => dispatch(actions.signout())
-  }
-}
-
-Signout.propTypes = {
-  onSignout: PropTypes.func.isRequired
-}
-
-export default connect(null, mapDispatchToProps)(Signout)
+export default Signout
