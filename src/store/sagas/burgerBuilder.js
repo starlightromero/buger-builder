16576066d687d01797936b761aae90ccd9417a27
Starlight Romero <starlightromero@gmail.com>
2020-12-03 07:31:02 -0800

    changed axios to api

--- src/store/sagas/burgerBuilder.js
+++ src/store/sagas/burgerBuilder.js
@@ -1,10 +1,10 @@
-import axios from '../../axios-orders'
+import api from '../../api'
 import { put } from 'redux-saga/effects'
 import * as actions from '../actions'
 
 export function * initIngredientsSaga (action) {
   try {
-    const response = yield axios.get('ingredients.json')
+    const response = yield api.get('ingredients.json')
     yield put(actions.setIngredients(response.data))
   } catch (error) {
     yield put(actions.fetchIngredientsFail())
