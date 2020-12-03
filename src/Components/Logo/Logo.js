ca5984c1b905994899b422b54349cf481002388a
Starlight Romero <starlightromero@gmail.com>
2020-12-03 09:25:18 -0800

    changed image

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import BurgerLogo from '../../assets/images/burger-logo.png'
+import LogoImage from '../../assets/images/logo.png'
 import classes from './Logo.module.css'
 
 const Logo = props => (
   <div className={classes.Logo} style={{ height: props.height }}>
-    <img src={BurgerLogo} alt='MyBurger' />
+    <img src={LogoImage} alt='JWarners' />
   </div>
 )
 
