ffff57c32ce057cea32240d50303330600e7615b
Starlight Romero <starlightromero@gmail.com>
2020-12-03 06:52:06 -0800

    moved buttons

--- src/Components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/Components/Order/CheckoutSummary/CheckoutSummary.js
@@ -4,18 +4,16 @@ import classes from './CheckoutSummary.module.css'
 import Burger from '../../Burger/Burger'
 import Button from '../../UI/Button/Button'
 
-const CheckoutSummary = props => {
-  return (
-    <div className={classes.CheckoutSummary}>
-      <h1>We hope it tastes great!</h1>
-      <div style={{ width: '100%', margin: 'auto' }}>
-        <Burger ingredients={props.ingredients} />
-      </div>
-      <Button btnType='Danger' clicked={props.checkoutCanceled}>CANCEL</Button>
-      <Button btnType='Success' clicked={props.checkoutContinued}>CONTINUE</Button>
+const CheckoutSummary = props => (
+  <div className={classes.CheckoutSummary}>
+    <h1>We hope it tastes great!</h1>
+    <Button btnType='Danger' clicked={props.checkoutCanceled}>CANCEL</Button>
+    <Button btnType='Success' clicked={props.checkoutContinued}>CONTINUE</Button>
+    <div style={{ width: '100%', margin: 'auto', height: 'auto' }}>
+      <Burger ingredients={props.ingredients} />
     </div>
-  )
-}
+  </div>
+)
 
 CheckoutSummary.propTypes = {
   ingredients: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
